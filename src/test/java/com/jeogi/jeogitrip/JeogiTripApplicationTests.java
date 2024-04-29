32fdbfc2e6c3451d6dec2746b237e962bf9389ee
taegyuni <taegyun7553@naver.com>
2024-04-30 00:29:28 +0900

    Env:application 설정 추가

---
+++ src/test/java/com/jeogi/jeogitrip/JeogiTripApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jeogi.jeogitrip;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JeogiTripApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
