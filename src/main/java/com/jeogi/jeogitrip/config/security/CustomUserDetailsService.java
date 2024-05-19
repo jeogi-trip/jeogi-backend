02be15ed112646bedb9ec2bfc6ee7314c7e43efc
taegyuni <taegyun7553@naver.com>
2024-05-20 00:05:01 +0900

    Feat: Spring Security 설정 추가

--- src/main/java/com/jeogi/jeogitrip/config/security/CustomUserDetailsService.java
+++ src/main/java/com/jeogi/jeogitrip/config/security/CustomUserDetailsService.java
@@ -1,2 +1,36 @@
-package com.jeogi.jeogitrip.config.security;public class CustomUserDetailsService {
+package com.jeogi.jeogitrip.config.security;
+
+import com.jeogi.jeogitrip.user.model.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class CustomUserDetailsService implements UserDetailsService {
+    private final UserService userService;
+    private final PasswordEncoder passwordEncoder;
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        com.jeogi.jeogitrip.user.model.User user = userService.getUserByEmail(email);
+        if (user == null) {
+            throw new UsernameNotFoundException("User not found with email: " + email);
+        }
+        return createUserDetails(user);
+
+
+    }
+    private UserDetails createUserDetails(com.jeogi.jeogitrip.user.model.User user) {
+        return User.builder()
+                .username(user.getEmail())
+                .password(passwordEncoder.encode(user.getUserId().toString()))
+                .authorities(new SimpleGrantedAuthority(user.getRole()))
+                .build();
+    }
 }
--- src/main/java/com/jeogi/jeogitrip/config/security/SecurityConfig.java
+++ src/main/java/com/jeogi/jeogitrip/config/security/SecurityConfig.java
@@ -1,2 +1,51 @@
-package com.jeogi.jeogitrip.config.security;public class SecurityConfig {
+package com.jeogi.jeogitrip.config.security;
+
+import com.jeogi.jeogitrip.config.jwt.JwtAccessDeniedHandler;
+import com.jeogi.jeogitrip.config.jwt.JwtAuthenticationEntryPoint;
+import com.jeogi.jeogitrip.config.jwt.JwtFilter;
+import com.jeogi.jeogitrip.config.jwt.JwtProvider;
+import lombok.AllArgsConstructor;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+
+@Configuration
+@RequiredArgsConstructor
+public class SecurityConfig {
+
+    private final JwtProvider jwtProvider;
+    private final JwtAuthenticationEntryPoint jwtAuthenticationEntryPoint;
+    private final JwtAccessDeniedHandler jwtAccessDeniedHandler;
+
+    @Bean
+    PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    protected SecurityFilterChain config(HttpSecurity httpSecurity)throws Exception{
+        return httpSecurity
+                .csrf(AbstractHttpConfigurer::disable)
+                .addFilterBefore(new JwtFilter(jwtProvider), UsernamePasswordAuthenticationFilter.class)
+
+                .exceptionHandling(exceptionHandling -> exceptionHandling
+                        .authenticationEntryPoint(jwtAuthenticationEntryPoint)
+                        .accessDeniedHandler(jwtAccessDeniedHandler)
+                )
+                .sessionManagement(sessionManagement -> sessionManagement
+                        .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                )
+                .authorizeRequests(authorizeRequests -> authorizeRequests
+                        .anyRequest().permitAll()
+                )
+                .build();
+
+    }
 }
