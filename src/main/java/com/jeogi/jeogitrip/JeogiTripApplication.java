caa9a64d5b5dba08c9ec1fb159d75bccae568bd0
taegyuni <taegyun7553@naver.com>
2024-05-20 00:07:13 +0900

    Feat: 설정 추가

--- src/main/java/com/jeogi/jeogitrip/JeogiTripApplication.java
+++ src/main/java/com/jeogi/jeogitrip/JeogiTripApplication.java
@@ -2,8 +2,10 @@ package com.jeogi.jeogitrip;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
+@PropertySource("classpath:secure.properties")
 public class JeogiTripApplication {
 
     public static void main(String[] args) {
